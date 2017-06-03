d81f748cc11532d3e91afb28ce08eb243947f4b6
DESKTOP-8FPQISN\dell <gov hengly>
2017-06-03 17:32:47 +0700

    k

--- createDBF.php
+++ createDBF.php
@@ -18,12 +18,47 @@ $Sizee=$_REQUEST['Sizee'];
 $Resolution=$_REQUEST['Resolution'];
 $Multitouch=$_REQUEST['Multitouch'];
 $Protection=$_REQUEST['Protection'];
+$OS=$_REQUEST['OS'];
+$Chipset=$_REQUEST['Chipset'];
+$CPU=$_REQUEST['CPU'];
+$GPU=$_REQUEST['GPU'];
+$Card=$_REQUEST['Card'];
+$Internal=$_REQUEST['Internal'];
+$Primary=$_REQUEST['Primary'];
+$Features=$_REQUEST['Features'];
+$Video=$_REQUEST['Video'];
+$Secondary=$_REQUEST['Secondary'];
+$Alert=$_REQUEST['Alert'];
+$Loundspeaker=$_REQUEST['Loundspeaker'];
+$jack=$_REQUEST['jack'];
+$WLAN=$_REQUEST['WLAN'];
+$Bluetooth=$_REQUEST['Bluetooth'];
+$GPS=$_REQUEST['GPS'];
+$NFC=$_REQUEST['NFC'];
+$Radio=$_REQUEST['Radio'];
+$USB=$_REQUEST['USB'];
+$Sensors=$_REQUEST['Sensors'];
+$Messaging=$_REQUEST['Messaging'];
+$Brower=$_REQUEST[ 'Brower'];
+$Java=$_REQUEST['Java'];
+$Colors=$_REQUEST['Colors'];
+$Battery=$_REQUEST['Battery'];
+
 
 //    $ki = $_POST['hengly'];
 //    $sql = "insert into test (hi) values ('$ki')";
 
-$sql = "insert into network (Technology,TwoGBrand,ThreeGBrand,FourGBrand,Speed,GPRS,EDGE,Announced,Status,Dimensions,Weight,Build,SIM,Typee,Sizee,Resolution,Multitouch,Protection)
-values('$technology','$tGBrand','$thGBrand','$fGBrand','$Speed','$GPRS','$EDGE','$Announced','$Status','$Dimensions','$Weight','$Build','$SIM','$Typee','$Sizee','$Resolution','$Multitouch','$Protection')";
+$sql = "insert into network (Technology,TwoGBrand,ThreeGBrand,FourGBrand,
+Speed,GPRS,EDGE,Announced,Status,Dimensions,Weight,Build,SIM,Typee,Sizee,
+Resolution,Multitouch,Protection,OS,Chipset,CPU,GPU,Card,Internal,Primaryy,
+Features,Video,Secondary,Alert,Loundspeaker,jack,WLAN,Bluetooth,GPS,NFC,Radio,
+USB,Sensors,Messaging,Brower,Java,Colors,Battery)
+values('$technology','$tGBrand','$thGBrand','$fGBrand','$Speed','$GPRS','$EDGE',
+'$Announced','$Status','$Dimensions','$Weight','$Build','$SIM','$Typee','$Sizee',
+'$Resolution','$Multitouch','$Protection','$OS','$Chipset','$CPU','$GPU','$Card',
+'$Internal','$Primaryy','$Features','$Video','$Secondary','$Alert','$Loundspeaker',
+'$jack','$WLAN','$Bluetooth','$GPS','$NFC','$Radio','$USB','$Sensors','$Messaging',
+'$Brower','$Java','$Colors','$Battery')";
 
     $result = $conn->query($sql);
     if ($result) {
--- detail.php
+++ detail.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="phoneshop.css">
+    <link rel="stylesheet" type="text/css" href="Phoneshop.css">
 </head>
 <body style="background-color: #f0f0f5;">
 <nav class="navbar navbar-default">
@@ -65,49 +65,102 @@
         </div>
     </div>
 </div>
-<div class="container col-md-11  col-sm-11 col-xs-11 " id="container">
-    <div class="panel col-md-2 col-xs-2" style="background-color: #f0f0f5">
+<div class="container col-md-11  col-sm-11 col-xs-11 " id="container" >
+    <div class="col-md-12">
+        <div class="panel col-md-2 col-xs-2" style="background-color: #f0f0f5;">
 
-    </div>
-
-    <div class="thumbnail panel panel-default  col-md-8 col-xs-8 col-sm-8" >
-        <div>
-            <form action="Network.php">
-                <button type="network">Add</button>
-            </form>
-            <?php
-                require_once ('dbconf.php');
-                $sql="select *from network order by id";
-                $result = $conn->query($sql);
-                $count=$result->num_rows;
-                if($count>0){
-                    while ($row=$result->fetch_object()){
-                        echo "<div class='col-md-12' style='border: 1px solid'>
-                                <div class='col-md-5'>Technology</div>
-                                    <div class='col-md-7' style='border: 1px solid'>
-                                        <div class='panel page-header' style='background: red'>Technology: $row->Technology</div>
-                                        <div>2GBrand: $row->TwoGBrand</div>
-                                        <div>$row->ThreeGBrand</div>
-                                        <div>$row->FourGBrand</div>
-                                        <div>$row->Speed</div>
-                                        <div>$row->GPRS</div>
-                                        <div>$row->EDGE</div>
-                                        <div>$row->Announced</div>
-                                        <div>$row->Status</div>
-                                        <div>$row->Dimensions</div>
-                                        <div>$row->Weight</div>
-                                    </div>
-                                 </div>
-                                 
-                                 
-                                 
-                                 
-                                 ";
-
-                    }
-                }
-            ?>
         </div>
+
+        <div class="thumbnail panel panel-default  col-md-8 col-xs-8 col-sm-8" >
+            <div class="col-md-12" >
+                <form action="Network.php" >
+                    <button type="network">Add</button>
+                </form>
+                <table class="col-md-12">
+                    <?php
+                        require_once ('dbconf.php');
+                        $sql="select *from network order by id";
+                        $result = $conn->query($sql);
+                        $count=$result->num_rows;
+                        if($count>0){
+                            while ($row=$result->fetch_object()){
+
+                                echo "<div class='col-md-12'>
+                
+                                                <tr >
+                                                    <td rowspan='7' style='width: 20%'><div>Network</div></td>
+                                                    <td style='width: 20%'><div>Technology</div></td>
+                                                    <td >
+                                                        <div >$row->Technology</div>
+                                                    </td>
+                                                </tr>
+                                                <tr><td>2GBrand</td><td><div>$row->TwoGBrand</div></td></tr>
+                                                <tr><td>3GBrand</td><td><div>$row->ThreeGBrand</div></td></tr>
+                                                <tr><td>4GBrand</td><td><div>$row->FourGBrand</div></td></tr>
+                                                <tr><td>Speed</td><td><div>$row->Speed</div></td></tr>
+                                                <tr><td>GPRS</td><td><div>$row->GPRS</div></td></tr>
+                                                <tr><td>EDGE</td><td><div>$row->EDGE</div></td></tr>
+                                                
+                                                <tr><td rowspan='2'>Launch</td><td>Announced</td><td><div>$row->Announced</div></td></tr>
+                                                <tr><td>Status</td><td><div>$row->Status</div></td></tr>
+                                               
+                                                <tr><td rowspan='4'>Body</td><td>Dimensions</td><td><div>$row->Dimensions</div></td></tr>
+                                                <tr><td>Weight</td><td><div>$row->Weight</div></td></tr>
+                                                <tr><td>Build</td><td><div>$row->Build</div></td></tr>
+                                                <tr><td>SIM</td><td><div>$row->SIM</div></td></tr>
+                                                
+                                                <tr><td rowspan='5'>Display</td><td>Type</td><td><div>$row->Typee</div></td></tr>
+                                                <tr><td>Size</td><td><div>$row->Sizee</div></td></tr>
+                                                <tr><td>Resolution</td><td><div>$row->Resolution</div></td></tr>
+                                                <tr><td>Multitouch</td><td><div>$row->Multitouch</div></td></tr>
+                                                <tr><td>Protection</td><td><div>$row->Protection</div></td></tr>
+                                               
+                                                <tr><td rowspan='4'>Platform</td><td>OS</td><td><div>$row->OS</div></td></tr>
+                                                <tr><td>Chipset</td><td><div>$row->Chipset</div></td></tr>
+                                                <tr><td>CPU</td><td><div>$row->CPU</div></td></tr>
+                                                <tr><td>GPU</td><td><div>$row->GPU</div></td></tr>
+                                               
+                                                <tr><td rowspan='2'>Memory</td><td>Card slot</td><td><div>$row->Card</div></td></tr>
+                                                <tr><td>Internal</td><td><div>$row->Internal</div></td></tr>
+                                               
+                                                <tr><td rowspan='4'>Camera</td><td>Primary</td><td><div>$row->Primaryy</div></td></tr>
+                                                <tr><td>Features</td><td><div>$row->Features</div></td></tr>
+                                                <tr><td>Video</td><td><div>$row->Video</div></td></tr>
+                                                <tr><td>Secondary</td><td><div>$row->Secondary</div></td></tr>
+                                               
+                                                <tr><td rowspan='3'>Sound</td><td>Alert types</td><td><div>$row->Alert</div></td></tr>
+                                                <tr><td>Loundspeaker</td><td><div>$row->Loundspeaker</div></td></tr>
+                                                <tr><td>3.5mm jack</td><td><div>$row->jack</div></td></tr>
+                                                
+                                                <tr><td rowspan='6'>Comms</td><td>WLAN</td><td><div>$row->WLAN</div></td></tr>
+                                                <tr><td>Bluetooth</td><td><div>$row->Bluetooth</div></td></tr>
+                                                <tr><td>GPS</td><td><div>$row->GPS</div></td></tr>
+                                                <tr><td>NFC</td><td><div>$row->NFC</div></td></tr>
+                                                <tr><td>Radio</td><td><div>$row->Radio</div></td></tr>
+                                                <tr><td>USB</td><td><div>$row->USB</div></td></tr>
+                                                
+                                                
+                                                <tr><td rowspan='4'>Features</td><td>Sensors</td><td><div>$row->Sensors</div></td></tr>
+                                                <tr><td>Messaging</td><td><div>$row->Messaging</div></td></tr>
+                                                <tr><td>Browser</td><td><div>$row->Brower</div></td></tr>
+                                                <tr><td>Java</td><td><div>$row->Java</div></td></tr>
+                                                <tr><td>Battery</td><td></td><td><div>$row->Battery</div></td></tr>
+                                                <tr><td>Music</td><td>Colors</td><td><div>$row->Colors</div></td></tr>
+                                                
+                                                
+                                                
+                                                
+                                            </div>
+      
+                                         ";
+
+                            }
+                        }
+                    ?>
+                </table>
+            </div>
+        </div>
+
 
     </div>
 </div>
