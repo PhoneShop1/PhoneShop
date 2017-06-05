630fa41013b8d3c2a643bc96d3181adc64e009d4
DESKTOP-MSEFR8B\THIRIN <patputhirin@gmail.com>
2017-06-06 00:23:07 +0700

    "test"

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="phoneshop.css">
     <script src="phoneshop.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body style="background-color: #f0f0f5;">
 	<nav class="navbar navbar-default">
