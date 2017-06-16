8db2d7e2057733827c3970b18423c88fea6d3315
DESKTOP-MSEFR8B\THIRIN <patputhirin@gmail.com>
2017-06-16 16:46:25 +0700

    AboutUs page

--- index.php
+++ index.php
@@ -118,28 +118,52 @@
 		</div>
 		</div>
 	</div>
+    <div id="carousel-example-generic" class="carousel slide col-md-12" data-ride="carousel" style="background: none;">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox" >
+            <div class="item active">
+                <img src="img/p10show.jpg" alt="...">
+                <div class="carousel-caption">
+                    ...
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/p10show.jpg" alt="...">
+                <div class="carousel-caption">
+                    ...
+                </div>
+            </div>
+            ...
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev" style="background: none;">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next" style="background: none;">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 	<div class="container col-md-11  col-sm-11 col-xs-11 " id="container">
 		<div class="panel col-md-2 col-xs-2" style="background-color: #f0f0f5">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h3 class="panel-title" id="menu">Phones</h3>
-				</div>
-				<div class="panel-body">
-					Samsung
-				</div>
-				<div class="panel-body">
-					Huawei
-				</div>
-				<div class="panel-body">
-					Oppo
-				</div>
-				<div class="panel-body">
-					Sugar
-				</div>
-				<div class="panel-body">
-					Iphone
-				</div>
-			</div>
+            <div class="list-group">
+                <a href="#" class="list-group-item active">
+                    Cras justo odio
+                </a>
+                <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+                <a href="#" class="list-group-item">Morbi leo risus</a>
+                <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                <a href="#" class="list-group-item">Vestibulum at eros</a>
+            </div>
 		</div>
         <div class="thumbnail panel panel-default  col-md-8 col-xs-8 col-sm-8" >
 			<div class="thumbnail panel panel-default  col-md-12 col-xs-12 col-sm-12" >
@@ -432,25 +456,14 @@
             </div>
         </div>
         <div class="panel col-md-2 col-xs-2" style="background-color: #f0f0f5">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title" id="menu">Phones</h3>
-                </div>
-                <div class="panel-body">
-                    Samsung
-                </div>
-                <div class="panel-body">
-                    Huawei
-                </div>
-                <div class="panel-body">
-                    Oppo
-                </div>
-                <div class="panel-body">
-                    Sugar
-                </div>
-                <div class="panel-body">
-                    Iphone
-                </div>
+            <div class="list-group">
+                <a href="#" class="list-group-item active">
+                    Cras justo odio
+                </a>
+                <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+                <a href="#" class="list-group-item">Morbi leo risus</a>
+                <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                <a href="#" class="list-group-item">Vestibulum at eros</a>
             </div>
         </div>
         </div>
