b66199b57fe3bab27db610a4f19b1c367788526e
DESKTOP-8FPQISN\dell <gov hengly>
2017-07-06 21:47:17 +0700

    Can add product and show first display by name of product

--- huawei.php
+++ huawei.php
@@ -10,60 +10,162 @@
 	<link rel="stylesheet" type="text/css" href="phoneshop.css">
 </head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">HengLay PhoneShop</a>
-			</div>
+<nav class="navbar navbar-default" style="background-color: #b3e0ff;">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">HengLay PhoneShop</a>
+            <h5 class="navbar-header" style="margin-top: 20px;">Tel: 077 465615</h5>
+        </div>
+        <div class="navbar-header ">
 
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				
-				<form class="navbar-form navbar-right">
-					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Search">
-					</div>
-					<button type="submit" class="btn btn-default">Search</button>
-				</form>
-				<!-- <ul class="nav navbar-nav navbar-right">
-					
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Action</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="#">Separated link</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="#">One more separated link</a></li>
-						</ul>
-					</li>
-				</ul> -->
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
-	</nav>
-	<div class="container col-md-12 col-xs-12 col-sm-12" id="r">
-		<div class=" col-md-6 col-xs-6" >
-			<img src="img/hl.png">
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-		</div>
-		<div class=" col-md-6 col-xs-6 col-sm-6" style="text-align: right;>
-			<h3 style="color: black;">Heng Lay Phone Shop</h3>
-			<h3 style="color: black;">More information:
-			</h3>
-			<h3><a href="khmertypingonline@gmail.com">khmertypingonline@gmail.com</a></h3>
-			<h3 style="color: black;">Tel: 077 465 615</h3>
+            <form class="navbar-form navbar-right">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Search</button>
+            </form>
+            <div class="nav navbar-nav navbar-right"s>
+                <form class="navbar-form navbar-right">
+                    <?php
+                    session_start();
 
-		</div>
-	</div>
+                    if(isset($_COOKIE['username'])){
+                        echo $_COOKIE['username'];
+                    }
+                    if(isset($_SESSION['username'])){
+                        echo $_GET['name'];
+                    }else{
+                        echo "";
+                    }
+                    ?>
+                </form>
+            </div>
+            </a>
+            <ul class="nav navbar-nav navbar-right">
+                <!--                    <p><a href="LogOut.php" class="btn btn-info btn-block" id="myBtn">Log Out</a></p>-->
+                <form class="navbar-form navbar-right">
+                    <div >
+                        <a href="logout.php"> <button type="button" class="btn btn-default ">Log out</button></a>
+                    </div>
+                </form>
+                <li class="dropdown">
+                    <!--                        <button type="button" class="btn btn-default btn-lg"><a href="LogOut.php">log out</a></button>-->
+                    <form class="navbar-form navbar-right">
+                        <div >
+                            <button type="button" class="btn btn-default" id="myBtn">Login</button>
+                        </div>
+
+                    </form>
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal" role="dialog">
+                        <div id="login-overlay" class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+                                    <h4 class="modal-title" id="myModalLabel">Login to henglay.com</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="row">
+                                        <div class="col-xs-6">
+                                            <div class="well">
+                                                <form role="form" id="loginForm" method="POST" action="login.php?op=in" novalidate="novalidate">
+                                                    <div class="form-group">
+                                                        <label for="username" class="control-label">Username</label>
+                                                        <input type="text" class="form-control" id="username" name="username" value="" required="" title="Please enter you username" placeholder="Username">
+                                                        <span class="help-block"></span>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="password" class="control-label">Password</label>
+                                                        <input type="password" class="form-control" id="password" name="password" value="" required="" title="Please enter your password">
+                                                        <span class="help-block"></span>
+                                                    </div>
+                                                    <div id="loginErrorMsg" class="alert alert-error hide">Wrong username og password</div>
+                                                    <div class="checkbox">
+                                                        <label>
+                                                            <input type="checkbox" name="r" id="remember"> Remember login
+                                                        </label>
+                                                        <p class="help-block">(if this is a private computer)</p>
+                                                    </div>
+                                                    <button type="submit" class="btn btn-success btn-block">Login</button>
+                                                    <a href="/forgot/" class="btn btn-default btn-block">Help to login</a>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <div class="col-xs-6">
+                                            <p class="lead">Register now for <span class="text-success">FREE</span></p>
+                                            <ul class="list-unstyled" style="line-height: 2">
+                                                <li><span class="fa fa-check text-success"></span> See all your orders</li>
+                                                <li><span class="fa fa-check text-success"></span> Fast re-order</li>
+                                                <li><span class="fa fa-check text-success"></span> Save your favorites</li>
+                                                <li><span class="fa fa-check text-success"></span> Fast checkout</li>
+                                                <li><span class="fa fa-check text-success"></span> Get a gift <small>(only new customers)</small></li>
+                                                <li><a href="/read-more/"><u>Read more</u></a></li>
+                                            </ul>
+                                            <p><a href="register.php" class="btn btn-info btn-block">Yes please, register now!</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+<div id="carousel-example-generic" class="carousel slide col-md-12" data-ride="carousel" style="background: none; margin-top: 50px;">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner col-md-8 col-sm-8 col-xs-8 col-md-offset-2 col-sm-offset-2 col-xs-offset-2" role="listbox" >
+        <div class="item active">
+            <img src="img/Location.png" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        <div class="item">
+            <img src="img/p10show1.jpg" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        <div class="item">
+            <img src="img/s8show1.jpg" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        ...
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev" style="background: none;">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next" style="background: none;">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
 	<div class="container col-md-11  col-sm-11 col-xs-11 " id="container">
 		<div class="panel col-md-2 col-xs-2" >
 			<div class="panel panel-default">
